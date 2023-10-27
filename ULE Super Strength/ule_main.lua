4451e3ca88a648a9fd5fcd4afd0573331373b26a
alekseiM88 <136792894+alekseiM88@users.noreply.github.com>
2023-10-27 17:24:52 -0500

    Super strength code cleanup and bug fixes.

--- ULE Super Strength/ule_main.lua
+++ ULE Super Strength/ule_main.lua
@@ -53,13 +53,27 @@ function SetPlayerTool(toolName)
 end
 
 function DropBody()
+    if heldBody == nil then return end
+    
     heldBody = nil
     SetPlayerTool(currentTool)
 end
 
-function AttemptGrab()
+function ThrowBody()
+    if heldBody == nil then return end
 
-    hasThrown = false
+    local bodyTf = GetBodyTransform(heldBody)
+    local globalHeldPosition = TransformToParentPoint(bodyTf, heldPosition)
+    ApplyBodyImpulse(heldBody, globalHeldPosition, VecScale(cameraForward, 40 * GetBodyMass(heldBody)))
+    
+    heldBody = nil
+    
+    hasThrown = true
+end
+
+function AttemptGrab()
+    -- don't attempt to grab if we're still post throw
+    if hasThrown then return end
 
     local hit, dist, normal, shape = QueryRaycast(cameraTf.pos, cameraForward, 5)
     
@@ -71,9 +85,11 @@ function AttemptGrab()
     
     heldBody = GetShapeBody(shape)
     
-    if heldBody == GetWorldBody() then -- grabbing a chunk of the world, try to break it off
+    -- grabbing a chunk of a static body, try to break it off
+    if not IsBodyDynamic(heldBody) then 
         local hitPosition = VecAdd(VecScale(cameraForward, dist), cameraTf.pos)
         local queryBounds = Vec(HOLERADIUS,HOLERADIUS,HOLERADIUS)
+        
         -- get all bodies in ~radius of hitposition, then make hole and lump all new bodies into a single body, then grab that body
         local oldBodies = QueryAabbBodies(VecSub(hitPosition, queryBounds), VecAdd(hitPosition, queryBounds))
         
@@ -112,7 +128,8 @@ function AttemptGrab()
         
     end
         
-    if IsBodyDynamic(heldBody) then -- regular body grabbing
+    -- regular body grabbing
+    if IsBodyDynamic(heldBody) then 
     
         local bodyTf = GetBodyTransform(heldBody)
         local globalHeldPosition = VecAdd(VecScale(cameraForward, dist), cameraTf.pos)
@@ -141,20 +158,21 @@ function tick(dt)
     cameraTf = GetPlayerCameraTransform(true)
     cameraForward = TransformToParentVec(cameraTf, Vec(0,0,-1))
 
-    -- get grabbed object
+    -- attempt to grab a body if grab is pressed
     if InputDown("grab") and GetBool("game.player.canusetool") then
-        if heldBody == nil and InputPressed("grab") then
+        if heldBody == nil and InputPressed("grab") and not InputDown("usetool") then
             AttemptGrab()
         end
-    elseif heldBody ~= nil and not hasThrown then
+    elseif heldBody ~= nil then
         DropBody()
     end
     
-    -- override tool
+    -- override held tool when grabbing or if post throw
     if heldBody ~= nil or hasThrown then
         SetPlayerTool("none")
     end
     
+   
     if heldBody ~= nil then
         SetBool("game.player.grabbing", true) 
         
@@ -163,16 +181,11 @@ function tick(dt)
         
         -- throwing
         if InputPressed("usetool") then
-            local bodyTf = GetBodyTransform(heldBody)
-            local globalHeldPosition = TransformToParentPoint(bodyTf, heldPosition)
-            ApplyBodyImpulse(heldBody, globalHeldPosition, VecScale(cameraForward, 40 * GetBodyMass(heldBody)))
-            
-            heldBody = nil
-            
-            hasThrown = true
+            ThrowBody()
         end
     end
     
+    -- end throw state if throw button is unpressed
     if hasThrown and not InputDown("usetool") then
         hasThrown = false
         SetPlayerTool(currentTool)
@@ -185,44 +198,43 @@ end
 
 
 function update(dt)
-    if heldBody ~= nil then
-        cameraTf = GetPlayerCameraTransform(true)
-        cameraForward = TransformToParentVec(cameraTf, Vec(0,0,-1))
+    if heldBody == nil then return end
     
-        local bodyTf = GetBodyTransform(heldBody)
-        local globalHeldPosition = TransformToParentPoint(bodyTf, heldPosition)
+    
+    cameraTf = GetPlayerCameraTransform(true)
+    cameraForward = TransformToParentVec(cameraTf, Vec(0,0,-1))
+
+    local bodyTf = GetBodyTransform(heldBody)
+    local globalHeldPosition = TransformToParentPoint(bodyTf, heldPosition)
 
 
-        -- linear velocity
+    -- linear velocity
+    local bodyVelocity = GetBodyVelocityAtPos(heldBody, globalHeldPosition)
+    
+    local idealVelocity = VecSub(VecAdd(cameraTf.pos, VecScale(cameraForward, holdDistance)), globalHeldPosition)
+    
+    local bodyMass = GetBodyMass(heldBody)
+    
+    idealVelocity = VecScale(idealVelocity, bodyMass * 6)
+    
+    local impulseVector = VecSub(idealVelocity, VecScale(bodyVelocity, bodyMass/3))
+    
+    ApplyBodyImpulse(heldBody, globalHeldPosition, impulseVector)
+    
 
-        local bodyVelocity = GetBodyVelocityAtPos(heldBody, globalHeldPosition)
-        
-        local idealVelocity = VecSub(VecAdd(cameraTf.pos, VecScale(cameraForward, holdDistance)), globalHeldPosition)
-        
-        local bodyMass = GetBodyMass(heldBody)
-        
-        idealVelocity = VecScale(idealVelocity, bodyMass * 6)
-        
-        local impulseVector = VecSub(idealVelocity, VecScale(bodyVelocity, bodyMass/3))
-        
-        ApplyBodyImpulse(heldBody, globalHeldPosition, impulseVector)
-        
- 
-        -- angular velocity
-        local heldVehicle = GetBodyVehicle(heldBody)
-        if not (IsBodyJointedToStatic(heldBody) or #GetJointedBodies(heldBody) > 1) or (heldVehicle ~= 0 and GetVehicleBody(heldVehicle) == heldBody) then
+    -- angular velocity
+    local heldVehicle = GetBodyVehicle(heldBody)
+    if not (IsBodyJointedToStatic(heldBody) or #GetJointedBodies(heldBody) > 1) or (heldVehicle ~= 0 and GetVehicleBody(heldVehicle) == heldBody) then
 
-            local worldRotationTf = TransformToParentTransform(cameraTf, rotationTf)
-            ConstrainOrientation(heldBody, 0, bodyTf.rot, worldRotationTf.rot, 12.5)
-            
-            
-            rotationTf = Transform(Vec(), QuatSlerp(bodyTf.rot, worldRotationTf.rot, 0.98))
+        local worldRotationTf = TransformToParentTransform(cameraTf, rotationTf)
+        ConstrainOrientation(heldBody, 0, bodyTf.rot, worldRotationTf.rot, 12.5)
         
-            rotationTf = TransformToLocalTransform(cameraTf, rotationTf)
-        end
         
-
+        rotationTf = Transform(Vec(), QuatSlerp(bodyTf.rot, worldRotationTf.rot, 0.98))
+    
+        rotationTf = TransformToLocalTransform(cameraTf, rotationTf)
     end
+
 end
 
 
